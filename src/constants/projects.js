bacf0a44548cdccbd0e52267f7ca56580db9538d
Lina Budkova <linabudkova35@gmail.com>
2023-04-20 18:11:38 +0300

    add projects page

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,11 +1,17 @@
+import Wallet from '../img/wallet.png';
+import SlimMom from '../img/slim-mom.png';
+import Weather from '../img/weather.png';
+import Filmoteka from '../img/filmoteka.png';
+import Tasks from '../img/tasks.png';
+
 export const projects = [
   {
     id: 1,
     title: 'Wallet',
     description:
       'Wallet is a React application built with React Redux, Redux toolkit, React Hook Form, Yup for validation, react-i18next, and styled-components. It is an application that allows you to keep track of your income and expenses. You can log your expenses and track your monthly expenses and income with a visual graph. Additionally, it features a currency exchange rate page to keep track of different currency rates.',
-    preview_image: '',
-    technologies: [],
+    preview_image: Wallet,
+
     code: 'https://github.com/LinaEno/react-project',
     site: 'https://linaeno.github.io/react-project/',
   },
@@ -14,8 +20,8 @@ export const projects = [
     title: 'Slim Mom',
     description:
       'Slim Mom is a web app built using React, Redux, React Hook Form, Yup for validation, and styled-components. It helps users count their daily calorie intake by filling in a form based on their current and desired weight. Then, users can add the food they eat each day to the table, and the app calculates how many calories were consumed and how many more can still be eaten. The app also includes a chart that shows users daily calorie intake, and how it compares to their daily goal.',
-    preview_image: '',
-    technologies: [],
+    preview_image: SlimMom,
+
     code: 'https://github.com/LinaEno/slim-mom',
     site: 'https://linaeno.github.io/slim-mom/',
   },
@@ -24,8 +30,8 @@ export const projects = [
     title: 'Weather API',
     description:
       'Weather App is a React-based web application that allows users to view detailed weather information for their city. The app uses axios for API requests and includes a background video player. Users can view weather information for up to 5 days in advance and navigate between tabs to access different types of weather data',
-    preview_image: '',
-    technologies: [],
+    preview_image: Weather,
+
     code: 'https://github.com/LinaEno/Weather-App',
     site: 'https://linaeno.github.io/Weather-App/',
   },
@@ -35,7 +41,7 @@ export const projects = [
     description:
       'Youtube clone is a simple web application built using React, @mui/material, axios, react-player, and react-router-dom. It allows users to search for videos and channels on Youtube by title and view the results on the app. The app also allows users to watch videos directly on the app using the React Player component. The interface is simple and intuitive, making it easy for users to navigate and find the content they are looking for on Youtube.',
     preview_image: '',
-    technologies: [],
+
     code: 'https://github.com/LinaEno/youtube_clone',
     site: 'https://linaeno.github.io/youtube_clone/',
   },
@@ -44,8 +50,8 @@ export const projects = [
     title: 'Filmoteka',
     description:
       'The project is called Filmoteka and it was made using vanilla JavaScript. I used GSAP and Axios as well. It is a website with a collection of movies where you can search for popular movies, filter by ratings and genres, or search for movies by title. You can also add movies to your favorites or mark them as watched, which will be saved in local storage.',
-    preview_image: '',
-    technologies: [],
+    preview_image: Filmoteka,
+
     code: 'https://github.com/LinaEno/js-project-cyborgs',
     site: 'https://linaeno.github.io/js-project-cyborgs/index.html',
   },
@@ -55,7 +61,7 @@ export const projects = [
     description:
       'Gym fit is a React-based project that uses @mui/material, react-horizontal-scrolling-menu, and axios technologies. It features a variety of exercises for different muscle groups that can be searched by name or by selecting a specific body part (such as chest, back, or legs). Each exercise has a preview, and when selected, displays a full description, along with similar exercises for the same muscle groups and links to real videos featuring those exercises on YouTube.',
     preview_image: '',
-    technologies: [],
+
     code: 'https://github.com/LinaEno/gym_fit',
     site: 'https://linaeno.github.io/gym_fit/',
   },
@@ -64,8 +70,8 @@ export const projects = [
     title: 'Tasks manager',
     description:
       'Task Manager is a web application built with React, Emotion/Styled, and Redux Toolkit, among other technologies. It features user authentication and registration, with support for user profile pictures. The app allows users to manage their tasks, including the ability to view task history and active tasks. A chart is included to visualize the percentage of completed tasks compared to active ones. The app is also localized with i18next and is integrated with Firebase to store task data. Additionally, the app is responsive and can be used on different devices.',
-    preview_image: '',
-    technologies: [],
+    preview_image: Tasks,
+
     code: 'https://github.com/LinaEno/daily-task-manager',
     site: 'https://linaeno.github.io/daily-task-manager/',
   },
@@ -75,7 +81,7 @@ export const projects = [
     description:
       'The game is called "Fruits Game" and is built using React. The game board consists of an 8x8 grid with six different types of fruits appearing randomly. The objective of the game is to match three or four fruits of the same type in a row, earning points as the matched fruits disappear and new ones appear on the board.',
     preview_image: '',
-    technologies: [],
+
     code: 'https://github.com/LinaEno/fruit-game',
     site: 'https://linaeno.github.io/fruit-game/',
   },
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
 
+import { projects } from '../constants/projects';
+
 const Projects = () => {
-  return <div></div>;
+  return (
+    <div style={{ backgroundColor: '#000' }}>
+      <h1 style={{ color: '#fff' }}>My Projects</h1>
+      <div className="gridContainer">
+        {projects.map(
+          ({ id, preview_image, title, description, code, site }) => {
+            return (
+              <div key={id} className="blogCard">
+                <div className="imgWrapper">
+                  <img
+                    width="900px"
+                    height="455px"
+                    src={preview_image}
+                    alt="project"
+                    className="img"
+                  />
+                </div>
+                <div className="titleContent">
+                  <h3 title="true">{title}</h3>
+                  <hr className="hr" />
+                </div>
+                <p className="cardInfo">{description}</p>
+                <div className="utilityList">
+                  <a href={code} className="externalLinks">
+                    code
+                  </a>
+                  <a href={site} className="externalLinks">
+                    site
+                  </a>
+                </div>
+              </div>
+            );
+          }
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
