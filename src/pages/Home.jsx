de614936538b30803318fc39d6c2321335392056
Lina Budkova <linabudkova35@gmail.com>
2023-04-24 14:39:02 +0300

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Pannellum } from 'pannellum-react';
 
 import fantasyNight from '../img/fantasy_lands_forest_night.jpg';
 import fantasyCave from '../img/fantasy_lands_forest_night_river.jpg';
+import { Default, Mobile } from 'components/Media/Media';
 
 const Home = () => {
   const [currentScene, setCurrentScene] = useState(fantasyNight);
@@ -20,32 +21,63 @@ const Home = () => {
         >
           Download CV
         </a>
-        <Pannellum
-          width="100%"
-          height="100%"
-          image={currentScene}
-          yaw={300}
-          hfov={110}
-          autoLoad
-          autoRotate={-5}
-          compass={true}
-          showZoomCtrl={false}
-          mouseZoom={true}
-          onLoad={() => {
-            console.log('panorama loaded');
-          }}
-        >
-          <Pannellum.Hotspot
-            type="custom"
-            pitch={-10}
-            yaw={-120}
-            handleClick={(evt, name) =>
-              setCurrentScene(
-                currentScene !== fantasyNight ? fantasyNight : fantasyCave
-              )
-            }
-          />
-        </Pannellum>
+        <Mobile>
+          <Pannellum
+            width="100%"
+            height="100%"
+            image={currentScene}
+            yaw={300}
+            hfov={110}
+            autoLoad
+            autoRotate={-5}
+            compass={false}
+            showZoomCtrl={false}
+            mouseZoom={false}
+            scale={0.6}
+            onLoad={() => {
+              console.log('panorama loaded');
+            }}
+          >
+            <Pannellum.Hotspot
+              type="custom"
+              pitch={-10}
+              yaw={-120}
+              handleClick={(evt, name) =>
+                setCurrentScene(
+                  currentScene !== fantasyNight ? fantasyNight : fantasyCave
+                )
+              }
+            />
+          </Pannellum>
+        </Mobile>
+        <Default>
+          <Pannellum
+            width="100%"
+            height="100%"
+            image={currentScene}
+            yaw={300}
+            hfov={110}
+            autoLoad
+            autoRotate={-5}
+            compass={false}
+            showZoomCtrl={false}
+            mouseZoom={true}
+            onLoad={() => {
+              console.log('panorama loaded');
+            }}
+          >
+            <Pannellum.Hotspot
+              type="custom"
+              pitch={-10}
+              yaw={-120}
+              handleClick={(evt, name) =>
+                setCurrentScene(
+                  currentScene !== fantasyNight ? fantasyNight : fantasyCave
+                )
+              }
+            />
+          </Pannellum>
+        </Default>
       </div>
     </>
   );
